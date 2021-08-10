4168db9a2af6f7c10ca28a5a9025f6555aadcfc6
Stephen Liang <sdliang@usc.edu>
2021-08-09 22:06:36 -0500

    Update footer.vue

--- app/components/partials/footer.vue
+++ app/components/partials/footer.vue
@@ -8,15 +8,32 @@
         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><title>logo-instagram</title><g fill="#212121"><path d="M349.33 69.33a93.62 93.62 0 0 1 93.34 93.34v186.66a93.62 93.62 0 0 1-93.34 93.34H162.67a93.62 93.62 0 0 1-93.34-93.34V162.67a93.62 93.62 0 0 1 93.34-93.34h186.66m0-37.33H162.67C90.8 32 32 90.8 32 162.67v186.66C32 421.2 90.8 480 162.67 480h186.66C421.2 480 480 421.2 480 349.33V162.67C480 90.8 421.2 32 349.33 32z"></path><path d="M377.33 162.67a28 28 0 1 1 28-28 27.94 27.94 0 0 1-28 28z"></path><path d="M256 181.33A74.67 74.67 0 1 1 181.33 256 74.75 74.75 0 0 1 256 181.33m0-37.33a112 112 0 1 0 112 112 112 112 0 0 0-112-112z"></path></g></svg>
       </a>
     </div>
+
+    <ul class="flex flex-row items-center sm:mt-4 sm:pt-4 md:mt-0 md:pt-0">
+      <li
+        v-for="(page, index) in pages"
+        :key="index"
+        class="block font-medium px-4 py-1 md:p-2 lg:px-4"
+      >
+        <nuxt-link :to="`/${page.slug}`">{{ page.title }}</nuxt-link>
+      </li>
+    </ul>
+      
   </footer>
 </template>
 
+
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator';
-
 @Component
-export default class Footer extends Vue {}
+export default class Footer extends Vue {
+  get pages(): Page[] {
+    return this.$store.state.pages;
+  }
+}
 </script>
+
+
 
 <style lang="scss">
 .footer {
