b4a9c4340345668d03c0423b020064d2a4b61924
Stephen Liang <sdliang@usc.edu>
2021-08-09 21:48:38 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="home">
-    <div class="py-24 md:py-36 mx-auto flex flex-wrap flex-col md:flex-row items-center" v-bind:style="{ backgroundImage: 'url(' + header + ')';backgroundSize:cover }">
+    <div class="py-24 md:py-36 mx-auto flex flex-wrap flex-col md:flex-row items-center bg-fit" v-bind:style="{ backgroundImage: 'url(' + header + ')' }">
       <div class="flex flex-col w-full xl:w-3/5 justify-center lg:items-start overflow-y-hidden">
         <div v-html="$md.render(welcomeText)" class="home__welcome markdown" style="color:#fff" />
 
