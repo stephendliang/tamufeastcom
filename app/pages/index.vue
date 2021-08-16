838b44065356a7d2bcce97c4ccf0f1edb93b49c8
Stephen Liang <sdliang@usc.edu>
2021-08-15 21:27:29 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -10,7 +10,7 @@
     </div>
     
     
-    <div>
+    <div style="padding-top:54px">
       <div class="x">
         <div class="flex flex-col w-full xl:w-3/5 justify-center lg:items-start overflow-y-hidden">
           <div v-html="$md.render(welcomeText)" class="home__welcome markdown tc"/>
