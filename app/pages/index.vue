6b5d6a5f95c71c1bc1878d72aca798947b5904da
Stephen Liang <sdliang@usc.edu>
2021-08-15 19:40:45 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,7 +3,7 @@
     <div class="py-24 md:py-36 mx-auto flex flex-wrap flex-col md:flex-row items-center bg-fit" v-bind:style="{ backgroundImage: 'url(' + header + ')' }">
       <div class="x">
         <div class="flex flex-col w-full xl:w-3/5 justify-center lg:items-start overflow-y-hidden">
-          <div v-html="$md.render(taglineText)" class="home__welcome markdown" style="color:#fff" />
+          <div class="home__welcome markdown" style="color:#fff" />{{ taglineText }} </div>
           <div class="mb-12 xl:mb-0">
           </div>
         </div>
