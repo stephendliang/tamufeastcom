84d2887b428d0459efc2ed0c83266000f6d76a5b
Stephen Liang <sdliang@usc.edu>
2021-08-15 19:00:55 -0500

    Update default.vue

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <main :class="[$route.name]" class="main px-4 md:px-0">
-    <div class="container mx-auto">
+    <div class="mx-auto">
       <site-header />
       <nuxt class="nuxt" />
     </div>
