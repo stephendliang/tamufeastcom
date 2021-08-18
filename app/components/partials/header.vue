6a20fa9647b6dc5e1829c01bb10015699a36468c
Stephen Liang <sdliang@usc.edu>
2021-08-17 20:10:56 -0700

    Update header.vue

--- app/components/partials/header.vue
+++ app/components/partials/header.vue
@@ -11,8 +11,8 @@
                         <li 
             v-for="(page, index) in pages"
             :key="index"
-            class="block font-medium px-4 py-1 md:p-2 lg:px-4" class="nav-item">
-            <nuxt-link :to="`/${page.slug}`" style="color:#212121">{{ page.title }}</nuxt-link></li>
+            class="nav-item block font-medium px-4 py-1 md:p-2 lg:px-4" class="nav-item">
+            <nuxt-link :to="`/${page.slug}`" class="nav-link" style="color:#212121">{{ page.title }}</nuxt-link></li>
                         <li class="nav-item"><a class="nav-link" href="#portfolio">Portfolio</a></li>
                         <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
                         <li class="nav-item"><a class="nav-link" href="#team">Team</a></li>
