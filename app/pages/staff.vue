8a4aab4df8d35e1590116f7f33c1e8c6368847e3
Stephen Liang <sdliang@usc.edu>
2021-08-18 20:59:24 -0700

    Update staff.vue

--- app/pages/staff.vue
+++ app/pages/staff.vue
@@ -9,7 +9,7 @@
       </div>
 
       <div class="flex flex-wrap md:-mx-4 pb-20">
-        <div v-for="(post, index) in posts" :key="index" class="w-full md:w-1/2 my-4 md:px-4">
+        <div v-for="(post, index) in posts" :key="index" class="w-full">
           <div class="post">
             <h3 class="text-base font-light">{{ post.name }}</h3>
             <p class="text-base font-light">{{ post.content }}</p>
