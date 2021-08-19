889822ea5829a80c4b80b44baff7c43181ea1378
Stephen Liang <sdliang@usc.edu>
2021-08-18 19:47:59 -0700

    Update staff.vue

--- app/pages/staff.vue
+++ app/pages/staff.vue
@@ -1,9 +1,9 @@
 <template>
   <section class="blog">
     <div class="py-8 md:py-16 text-center">
-      <h1 class="text-lg md:text-xl lg:text-4xl xl:text-6xl">Blog</h1>
+      <h1 class="text-lg md:text-xl lg:text-4xl xl:text-6xl">Staff members</h1>
       <h2 class="text-base md:text-lg lg:text-xl xl:text-2xl">
-        Slow-carb messenger bag mlkshk fingerstache four dollar toast.
+      Here's who we work with.
       </h2>
     </div>
 
