f069c869a49086401d1e75d7fdb68a5aabeb396b
Stephen Liang <sdliang@usc.edu>
2021-08-19 20:54:32 -0700

    Update header.vue

--- app/components/partials/header.vue
+++ app/components/partials/header.vue
@@ -4,33 +4,29 @@
       <nuxt-link to="/"  class="navbar-brand" style="color: #fff;font-weight:600;font-size:24px;font-family:'Garamond', 'Georgia', 'Times New Roman', serif">
         {{ logo }}
       </nuxt-link>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    Menu
-                    <i class="fas fa-bars ms-1"></i>
-                </button>
-                <div class="collapse navbar-collapse mla" id="navbarResponsive">
-                
-      
-                    <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
-                      <li class="nav-item block font-medium py-1 md:py-2">
-                        <nuxt-link to="/" class="nav-link">Home</nuxt-link>
-                      </li>
-                      <li 
-                          v-for="(page, index) in pages"
-                          :key="index"
-                          class="nav-item block font-medium py-1 md:py-2">
-                          <nuxt-link :to="`/${page.slug}`" class="nav-link">{{ page.title }}</nuxt-link></li>
-                      <li class="nav-item block font-medium py-1 md:py-2">
-                        <nuxt-link to="/staff" class="nav-link" >Staff</nuxt-link>
-                      </li>
-                      <li class="nav-item block font-medium py-1 md:py-2">
-                        <nuxt-link to="/committees" class="nav-link" >Committees</nuxt-link>
-                      </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        Menu
+      </button>
+      <div class="collapse navbar-collapse mla" id="navbarResponsive">
+        <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
+          <li class="nav-item block font-medium py-1 md:py-2">
+            <nuxt-link to="/" class="nav-link">Home</nuxt-link>
+          </li>
+          <li 
+              v-for="(page, index) in pages"
+              :key="index"
+              class="nav-item block font-medium py-1 md:py-2">
+              <nuxt-link :to="`/${page.slug}`" class="nav-link">{{ page.title }}</nuxt-link></li>
+          <li class="nav-item block font-medium py-1 md:py-2">
+            <nuxt-link to="/staff" class="nav-link" >Staff</nuxt-link>
+          </li>
+          <li class="nav-item block font-medium py-1 md:py-2">
+            <nuxt-link to="/committees" class="nav-link" >Committees</nuxt-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script lang="ts">
